a8d6b6d52c69c536d4b28051d2d07b3ba7b70bbe
Malte Lange <malt_lang@web.de>
2021-02-09 10:48:04 +0100

    Datei umbenannt; Menü wird nicht angezeigt; Textfeld vorhanden

--- main.cc
+++ main.cc
@@ -3,7 +3,6 @@
 int main(int argc, char *argv[]) {
 
   GtkWidget *window;
-  GtkWidget *vbox;
 
   GtkWidget *menubar;
   GtkWidget *fileMenu;
@@ -21,6 +20,9 @@ int main(int argc, char *argv[]) {
   GtkWidget *ansiMi;
   GtkWidget *utf8Mi;
   GtkWidget *sep;
+  GtkWidget *text_view;
+  GtkWidget *scrolled_window;
+  GtkTextBuffer *buffer;
 
   gtk_init(&argc, &argv);
 
@@ -29,8 +31,12 @@ int main(int argc, char *argv[]) {
   gtk_window_set_default_size(GTK_WINDOW(window), 500, 400);
   gtk_window_set_title(GTK_WINDOW(window), "GTK Editor");
 
-  vbox = gtk_vbox_new(FALSE, 0);
-  gtk_container_add(GTK_CONTAINER(window), vbox);
+	buffer = gtk_text_buffer_new (NULL);
+	text_view = gtk_text_view_new_with_buffer (buffer);
+	gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (text_view), GTK_WRAP_WORD); 
+	
+	scrolled_window = gtk_scrolled_window_new (NULL, NULL);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC); 
 
   menubar = gtk_menu_bar_new();
   fileMenu = gtk_menu_new();
@@ -65,8 +71,13 @@ int main(int argc, char *argv[]) {
   gtk_menu_shell_append(GTK_MENU_SHELL(menubar), fileMi);
   gtk_menu_shell_append(GTK_MENU_SHELL(menubar), editMi);
   gtk_menu_shell_append(GTK_MENU_SHELL(menubar), formatMi);
-  gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
 
+    gtk_container_add (GTK_CONTAINER (scrolled_window), text_view);
+	gtk_container_set_border_width (GTK_CONTAINER (scrolled_window), 5);
+	gtk_container_add (GTK_CONTAINER (window), scrolled_window);
+
+    gtk_box_pack_start(GTK_SCROLLEDWINDOW(scrolled_window), menubar, FALSE, FALSE, 0);
+    
   g_signal_connect(G_OBJECT(window), "destroy",G_CALLBACK(gtk_main_quit), NULL);
 
   g_signal_connect(G_OBJECT(exitMi), "activate", G_CALLBACK(gtk_main_quit), NULL);
