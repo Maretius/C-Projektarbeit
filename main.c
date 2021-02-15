100d6d08fa8357c87955fa00b1086cf22cba4ff1
Malte <malt_lang@web.de>
2021-02-15 09:01:02 +0100

    main und zeichne_balken geändert

--- main.c
+++ main.c
@@ -4,19 +4,22 @@
 #include <curses.h>
 #include <math.h>
 
-void zeichne_balken(float score) {
-	int i, j;
+void zeichne_balken(int index, float score) {
+			
+	int i, j, score_round;
+	score_round = round(score*10);
+	attrset(A_NORMAL);
 	addch (ACS_ULCORNER);
-	for (i = 0; i < score; i++) {
+	for (i = 0; i < score_round; i++) {
 		addch (ACS_HLINE);
 	}
 	addch (ACS_URCORNER);
 	addch ('\n');
 	for (i = 0; i < 1; i++) {
-		for (j = 0; j <= score+1; j++) {
+		for (j = 0; j <= score_round+1; j++) {
 			if (j == 0) {
 				addch (ACS_VLINE);
-			} else if (j == score+1) {
+			} else if (j == score_round+1) {
 				addch (ACS_VLINE);
 				addch ('\n');
 			} else {
@@ -25,11 +28,14 @@ void zeichne_balken(float score) {
 		}
 	}
 	addch (ACS_LLCORNER);
-	for (i = 0; i < score; i++) {
+	for (i = 0; i < score_round; i++) {
 		addch (ACS_HLINE);
     }
 	addch (ACS_LRCORNER);
 	addch ('\n');
+	attrset(A_BOLD);
+	mvprintw (index*3-2, score_round+3, "%.2f", score);
+	mvprintw (index*3, 0, "");
 }
 
 double measure_time(int array_size)
@@ -51,7 +57,6 @@ double measure_time(int array_size)
 			zuweisen_time = ((double)zuweisen_clock) / CLOCKS_PER_SEC;
 			zuweisen_time_ges = zuweisen_time_ges + zuweisen_time;
 		}
-		// zeit_ges = ((double)malloc_clock / CLOCKS_PER_SEC) + zeit_zuweisen;
 		free(array);
 		return malloc_time + zuweisen_time_ges;
 	} else {
@@ -65,7 +70,6 @@ int main()
  	double zeitSum;
  	int numbers[] = {1000,2000,4000,8000,16000,32000,64000};
  	double score, diver;
-	int score_round;
   
 	initscr ();
 	// Cursor und Funktionstasten ein
@@ -76,16 +80,11 @@ int main()
 	for (int i = 1; i < 8; i++) {
 		diver = numbers[i-1] / 1000;
  		zeitSum = measure_time(numbers[i-1]) / diver;
- 		score = zeitSum * 10000;
+ 		score = zeitSum * 1000;
  		if (zeitSum != 0) {
- 			attrset(A_NORMAL);
-			score_round = round(score*10);
-			zeichne_balken(score_round);
-			attrset(A_BOLD);
-			mvprintw (i*3-2, score_round+3, "%.2f", score);
-			mvprintw (i*3, 0, "");
+			zeichne_balken(i, score);
 		} else {
-			mvprintw (i*3-2, score_round+3, "Kein freier Speicher vorhanden!");
+			mvprintw (i*3-2, 3, "Kein freier Speicher vorhanden!");
 		}
 	}
 	getch ();
